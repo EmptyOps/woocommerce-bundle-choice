715c3eb594cc088569f229f8962500b948eaebc5
CmdrMahesh <mahesh@emptyops.com>
2019-12-09 14:48:51 +0530

    travis updated

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -15,6 +15,8 @@ if ( ! $_tests_dir ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+echo "Enabling woocommerce......".PHP_EOL;
+
 activate_plugin('/home/travis/build/EmptyOps/woocommerce/woocommerce.php');
 
 class SampleTest extends WP_UnitTestCase {
