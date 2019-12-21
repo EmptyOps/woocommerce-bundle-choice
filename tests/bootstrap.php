34f49ad4fc6ade4b595d27c8f64a6120c886cf7a
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 09:51:14 +0530

    chage test file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,23 +16,10 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
 }
-
-// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 require_once $_tests_dir . '/includes/bootstrap.php';
 require_once $_tests_dir . '/includes/listener-loader.php';
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-
-	//Include all files from WP tests library.
-	//$include_path='/home/travis/build/EmptyOps/wptest/tests/phpunit/includes/';
-
-	activate_plugin('woocommerce/woocommerce.php');
-
-	//require dirname( dirname( dirname( __FILE__ ) ) ). '/woocommerce/tests/bootstrap.php';
+function _manually_load_plugin() {	
 	require dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';	
 }
-
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
