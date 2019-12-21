3eb4684e77fc64858ef6356edc95f83f566658bb
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 13:14:17 +0530

    chage test file

--- tests/test-init.php
+++ tests/test-init.php
@@ -4,6 +4,8 @@ require_once $wp_tests_dir . '/includes/functions.php';
 require_once $wp_tests_dir . '/includes/bootstrap.php';
 require_once $wp_tests_dir . '/includes/listener-loader.php';
 
+require_once dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';		
+
 activate_plugin('woocommerce/woocommerce.php');
 activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');		
 
