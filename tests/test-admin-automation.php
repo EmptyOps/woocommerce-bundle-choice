eaa1d41c4833ae7816dead7403aa0f4976a0cf15
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 13:45:05 +0530

    chage test file

--- tests/test-admin-automation.php
+++ tests/test-admin-automation.php
@@ -1,4 +1,8 @@
 <?php
+/*
+* Boilerplate - DO NOT TOUCH ANY LINES HERE.
+* FUTURE : TO BE MOVED TO COMMON FILE.
+*/
 $wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
 require_once $wp_tests_dir . '/includes/functions.php';
 require_once $wp_tests_dir . '/includes/bootstrap.php';
@@ -8,8 +12,13 @@ require_once dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';
 
 activate_plugin('woocommerce/woocommerce.php');
 activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');		
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-class InitTest extends WP_UnitTestCase {
+/**
+* Backend unit testing.
+*/
+class AdminAutomation extends WP_UnitTestCase {
 
 	public function test_woocommerce_exists(){		
 		$this->assertTrue( class_exists('WooCommerce') );
@@ -18,19 +27,7 @@ class InitTest extends WP_UnitTestCase {
 	public function test_automatic_install(){
 		
 		require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
-		require_once('data/sample_data.php');
-
-		echo constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php';// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-	    /*$index=0;
-	    $attributes=array();
-	    while(!empty($_POST['attr_value_'.$index])){
-	        if(!empty($_POST['attr_'.$index])){
-	          	$_atttriutes[$index]['name']=$_POST['attr_value_'.$index];
-	          	$attributes[]=$_atttriutes[$index]; 
-	        }
-	        $index++;
-	    }*/
+		require_once('data/sample_data.php');	    
 
 		$catat=new EO_WBC_CatAt();		
 		$category_result=$catat->create_category($_category);
