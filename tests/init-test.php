ca6c15ad7c057793fe5d2cfceae2a0a37553a01a
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 09:44:18 +0530

    chage test file

---
+++ tests/init-test.php
@@ -0,0 +1,16 @@
+<?php
+class InitTest extends WP_UnitTestCase {
+
+	public __construct(){
+		$_tests_dir = getenv( 'WP_TESTS_DIR' );
+		if ( ! $_tests_dir ) {
+			$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+		}
+		require_once $_tests_dir . '/includes/functions.php';
+		activate_plugin('woocommerce/woocommerce.php');
+	}
+
+	public function test_woocommerce_exists(){		
+		$this->assertTrue( class_exists('WooCommerce') );
+	}
+}
