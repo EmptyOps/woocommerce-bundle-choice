a21739d496ab064ab67ebccef83dd648d1c3e177
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 09:35:56 +0530

    chage test file

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,10 +8,10 @@
 /**
  * Sample test case.
  */
-/*$_tests_dir = getenv( 'WP_TESTS_DIR' );
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}*/
+}
 // Give access to tests_add_filter() function.
 //require_once $_tests_dir . '/includes/functions.php';
 
@@ -19,7 +19,7 @@ if ( ! $_tests_dir ) {
 
 //require_once '/tmp/wordpress/wp-content/plugins/woocommerce/woocommerce.php';
 
-//activate_plugin('woocommerce/woocommerce.php');
+activate_plugin('woocommerce/woocommerce.php');
 
 class SampleTest extends WP_UnitTestCase {
 
