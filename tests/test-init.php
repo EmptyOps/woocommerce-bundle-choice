35f2a4fa7e44b3bfbdac5f540a1d082367ccafda
CmdrMahesh <mahesh@emptyops.com>
2019-12-21 13:01:56 +0530

    chage test file

--- tests/test-init.php
+++ tests/test-init.php
@@ -1,12 +1,4 @@
 <?php
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-require_once $_tests_dir . '/includes/functions.php';
-activate_plugin('woocommerce/woocommerce.php');
-activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');
-
 class InitTest extends WP_UnitTestCase {
 
 	public function test_woocommerce_exists(){		
