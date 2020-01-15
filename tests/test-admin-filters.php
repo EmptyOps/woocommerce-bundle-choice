ab2ba00b0840deb406e9b7aa540aa29ea619df50
CmdrMahesh <mahesh@emptyops.com>
2020-01-15 14:26:41 +0530

    Update test case

--- tests/test-admin-filters.php
+++ tests/test-admin-filters.php
@@ -26,9 +26,11 @@ class AdminFilters extends WP_UnitTestCase {
 
 	public function test_filters(){
 
+		$GLOBALS['hook_suffix'] = isset($GLOBALS['hook_suffix']) ? $GLOBALS['hook_suffix'] : "";
+		
 		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_View_Head_Banner.php');
-		//require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_First_Filter_Table.php');
-		//require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_Second_Filter_Table.php');
+		require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_First_Filter_Table.php');
+		require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_Second_Filter_Table.php');
 		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/EO_WBC_View_Filter.php');
 
 		$FirstFilterTable = new EO_WBC_First_Filter_Table();
