8570639fcfa042849622b13451a45592ea71ccdc
CmdrMahesh <mahesh@emptyops.com>
2020-01-25 12:14:13 +0530

    test case update

--- tests/test-front-integration.php
+++ tests/test-front-integration.php
@@ -17,42 +17,6 @@ activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
-/*require_once('data/sample_data.php');*/	    
-
-/*$category_status = false;
-$attribute_status = false;
-$map_status = false;
-$product_status = false;*/
-
-/*if ( !function_exists( 'wc_check_if_attribute_name_is_reserved' ) ) { 
-    require_once '/includes/wc-attribute-functions.php'; 
-} */
-
-/*do_action('plugins_loaded');
-do_action('woocommerce_init');*/
-
-
-
-/*add_action('plugins_loaded',function() use(&$_product,&$_maps,&$_atttriutes,&$_category,&$_img_url,&$category_status,&$attribute_status,&$map_status,&$product_status){*/
-
-	/**
-	 * create table to store orders in a SETS form that are recived from customers
-	 */
-/*	global $wpdb;
-	$eo_wbc_order_map= $wpdb->prefix."eo_wbc_order_maps";
-	if($wpdb->get_var( "SHOW TABLES LIKE '$eo_wbc_order_map'" ) != $eo_wbc_order_map)
-	{
-	    $sql = "CREATE TABLE `$eo_wbc_order_map`( ";
-	    $sql .= "  `order_id`  int(11) NOT NULL, ";
-	    $sql .= "  `order_map` text NOT NULL, ";
-	    $sql .= "  PRIMARY KEY(`order_id`)";
-	    $sql .= ") ".$wpdb->get_charset_collate().";";
-	    require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
-	    dbDelta($sql);
-	}*/
-
-
-/*},0);*/
 
 /*
 * Backend unit testing.
