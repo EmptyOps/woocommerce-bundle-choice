abeed52b72462e89499499cd23cd2eb349f6b8ea
CmdrMahesh <mahesh@emptyops.com>
2020-01-27 17:32:59 +0530

    update test case

--- tests/test-frontend-category.php
+++ tests/test-frontend-category.php
@@ -26,13 +26,12 @@ class FrontendCategory extends WP_UnitTestCase {
 
 	public function test_category_data(){
 
+		global $wp_query;		
+		$wp_query->queried_object = get_term_by( 'slug', get_option('eo_wbc_first_slug'), 'product_cat');
+
 		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Admin/EO_WBC_Support.php');
 		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Frontend/EO_WBC_Breadcrumb.php');
-		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Frontend/EO_WBC_Category.php');
-
-		global $wp_query;
-		
-		$wp_query->queried_object = get_term_by( 'slug', get_option('eo_wbc_first_slug'), 'product_cat');
+		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Frontend/EO_WBC_Category.php');		
 
 		$LoadEO_WBC_Category = new EO_WBC_Category();
 
