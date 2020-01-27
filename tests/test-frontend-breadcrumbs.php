c02f902f8f36d0a688a877c35908c2229cf012dc
CmdrMahesh <mahesh@emptyops.com>
2020-01-27 16:13:51 +0530

    update test cases

---
+++ tests/test-frontend-breadcrumbs.php
@@ -0,0 +1,67 @@
+<?php
+/*
+* Boilerplate - DO NOT TOUCH ANY LINES HERE.
+* FUTURE : TO BE MOVED TO COMMON FILE.
+*/
+$wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+require_once $wp_tests_dir . '/includes/functions.php';
+require_once $wp_tests_dir . '/includes/bootstrap.php';
+require_once $wp_tests_dir . '/includes/listener-loader.php';
+
+require_once dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';		
+
+activate_plugin('woocommerce/woocommerce.php');
+activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');		
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+* Backend unit testing.
+*/
+class FrontendBreadcrumb extends WP_UnitTestCase {
+
+    public function test_woocommerce_exists(){		
+		$this->assertTrue( class_exists('WooCommerce') );
+	}
+
+	public function Breadcrumb(){
+
+		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Frontend/
+			EO_WBC_Breadcrumb.php');
+
+		$BreaadClass = new EO_WBC_Breadcrumb();
+
+		$eo_wbc_add_breadcrumb = $BreaadClass->eo_wbc_add_breadcrumb($step=1,$begin);
+
+		$this->assertNotFalse($eo_wbc_add_breadcrumb);
+		$this->assertIsString($eo_wbc_add_breadcrumb);
+		$this->assertNotNull($eo_wbc_add_breadcrumb);
+
+
+		$eo_wbc_breadcumb_first_html = $BreaadClass->eo_wbc_breadcumb_first_html($step,$order);
+
+		$this->assertNotFalse($eo_wbc_breadcumb_first_html);
+		$this->assertIsString($eo_wbc_breadcumb_first_html);
+		$this->assertNotNull($eo_wbc_breadcumb_first_html);
+
+		$eo_wbc_breadcumb_second_html = $BreaadClass->eo_wbc_breadcumb_second_html($step,$order);
+
+		$this->assertNotFalse($eo_wbc_breadcumb_second_html);
+		$this->assertIsString($eo_wbc_breadcumb_second_html);
+		$this->assertNotNull($eo_wbc_breadcumb_second_html);
+
+		$eo_wbc_breadcrumb_view_url = $BreaadClass->eo_wbc_breadcrumb_view_url($product_id,$order);
+
+		$this->assertNotFalse($eo_wbc_breadcrumb_view_url);
+		$this->assertIsString($eo_wbc_breadcrumb_view_url);
+		$this->assertNotNull($eo_wbc_breadcrumb_view_url);
+
+		$eo_wbc_breadcrumb_get_category = $BreaadClass->eo_wbc_breadcrumb_get_category($product_id);
+
+		$this->assertNotFalse($eo_wbc_breadcrumb_get_category);
+		$this->assertIsString($eo_wbc_breadcrumb_get_category);
+		$this->assertNotNull($eo_wbc_breadcrumb_get_category);
+		
+	}
+
+}
---
+++ tests/test-frontend-product.php
@@ -0,0 +1,93 @@
+<?php
+/*
+* Boilerplate - DO NOT TOUCH ANY LINES HERE.
+* FUTURE : TO BE MOVED TO COMMON FILE.
+*/
+$wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+require_once $wp_tests_dir . '/includes/functions.php';
+require_once $wp_tests_dir . '/includes/bootstrap.php';
+require_once $wp_tests_dir . '/includes/listener-loader.php';
+
+require_once dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';		
+
+require_once 'db-setup.php';
+
+
+
+activate_plugin('woocommerce/woocommerce.php');
+activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');		
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+* Backend unit testing.
+*/
+class FrontendProduct extends WP_UnitTestCase {
+
+    public function test_woocommerce_exists(){		
+		$this->assertTrue( class_exists('WooCommerce') );
+	}
+
+	public function test_product_data(){
+
+		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
+		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Admin/EO_WBC_Support.php');
+		require_once(constant('EO_WBC_PLUGIN_DIR'). 'EO_WBC_Frontend/EO_WBC_Product.php');
+
+		global $post;
+
+		$EO_WBC_CatAt = new EO_WBC_CatAt();
+		require_once 'data/sample_data.php';
+		$create_products = $EO_WBC_CatAt->create_products($EO_WBC_CatAt->product);
+
+		$p = null;
+		$old_product = get_page_by_title('Setting #8800950587', OBJECT, 'product' );
+		if(!is_wp_error($old_product) and !empty($old_product))
+		{
+			if('publish' === get_post_status( $old_product->ID ))
+			{
+				$p = wc_get_product($old_product->ID);
+			}
+		}
+		
+		$post = $p;
+
+		$LoadEO_WBC_Product = new EO_WBC_Product();
+
+		$eo_wbc_category_link = $LoadEO_WBC_Product->eo_wbc_category_link($variable_status=FALSE);
+		$this->assertNotFalse($eo_wbc_category_link);
+		$this->assertNotNull($eo_wbc_category_link);
+		$this->assertContainsOnly($eo_wbc_category_link);
+
+
+		$make_pair_route = $LoadEO_WBC_Product->eo_wbc_make_pair_route();
+		$this->assertNotFalse($make_pair_route);
+		$this->assertNotNull($make_pair_route);
+		$this->assertContainsOnly($make_pair_route);
+
+		$make_pair = $LoadEO_WBC_Product->eo_wbc_make_pair();
+		
+		$wbc_config = $LoadEO_WBC_Product->eo_wbc_config();
+		
+		
+		$add_breadcrumb = $LoadEO_WBC_Product->eo_wbc_add_breadcrumb();
+		
+
+		$eo_wbc_render = $LoadEO_WBC_Product->eo_wbc_render();
+		
+		$product_route = $LoadEO_WBC_Product->eo_wbc_product_route();
+		$this->assertNotNull($product_route);
+		$this->assertNotFalse($product_route);
+		$this->assertContainsOnly($product_route);
+
+		$sub_categories =$LoadEO_WBC_Product->eo_wbc_sub_categories(get_option('eo_wbc_first_slug'));		
+		$this->assertIsArray($sub_categories);
+		$this->assertNotFalse($sub_categories);		
+
+		$get_category = $LoadEO_WBC_Product->eo_wbc_get_category();
+		$this->assertNotFalse($get_category);
+		$this->assertNotNull($get_category);
+		$this->assertIsString($get_category);
+	}
+
+}
