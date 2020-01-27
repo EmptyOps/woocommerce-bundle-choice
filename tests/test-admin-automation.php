5e79c84fef3318c97bcea5b444cc4bff9a820e1c
CmdrMahesh <mahesh@emptyops.com>
2020-01-27 16:02:29 +0530

    update test cases

--- tests/test-admin-automation.php
+++ tests/test-admin-automation.php
@@ -29,52 +29,50 @@ class AdminAutomation extends WP_UnitTestCase {
 
 	public function test_automatic_install(){
 		
-		global $_category;
-    	global $_atttriutes;
-    	global $_maps;
-    	global $_product;
-    	global $_img_url;
+			include_once WC_ABSPATH . 'includes/class-wc-product-factory.php';		
+			WC()->product_factory = new WC_Product_Factory();
 
-		require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
-			    
+        	global $_category;
+        	global $_atttriutes;
+        	global $_maps;
+        	global $_product;
+        	global $_img_url;
 
-		$catat=new EO_WBC_CatAt();		
-		$category_result=$catat->create_category($_category);
-		update_option('eo_wbc_cats',serialize($category_result));
-		$this->assertNotFalse($category_result);
-		$this->assertIsArray($category_result);
-		$this->assertEquals($category_result,unserialize(get_option('eo_wbc_cats')));
+        	do_action('woocommerce_init');   	
+        	
+        	require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
 
-		$attribute_result = $catat->create_attribute($_atttriutes);
-		update_option('eo_wbc_cats',serialize($attribute_result));
-		$this->assertNotFalse($attribute_result);
-		$this->assertIsArray($attribute_result);
-		$this->assertEquals($attribute_result,unserialize(get_option('eo_wbc_cats')));
+			$factory_object = new EO_WBC_CatAt();
+
+			$this->category_status =  !empty($factory_object->create_category($_category));
+			$this->attribute_status =  !empty($factory_object->create_attribute($_atttriutes));			
+			$this->map_status =  !empty($factory_object->add_maps($_maps));				
+			$this->product_status =  !empty($factory_object->create_products($_product));
+
+			update_option('eo_wbc_first_name','Diamond Shape');//FIRST : NAME
+            update_option('eo_wbc_first_slug','eo_diamond_shape_cat');//FIRST : SLUG
+            update_option('eo_wbc_first_url','/product-category/eo_diamond_shape_cat/');//FIRST : NAME
+            
+            update_option('eo_wbc_second_name','Setting Shape');//SECOND : NAME
+            update_option('eo_wbc_second_slug','eo_setting_shape_cat');//SECOND : SLUG
+            update_option('eo_wbc_second_url','/product-category/eo_setting_shape_cat/');//SECOND : URL   
+
+            update_option('eo_wbc_config_category',1);
+            update_option('eo_wbc_config_map',1);    
+            update_option('eo_wbc_btn_setting','0');
+            update_option('eo_wbc_btn_position','begining');
+
+            $this->assertTrue(class_exists( 'WooCommerce', false ));
+			$this->assertFalse(empty(WC()->product_factory));
+
+			//$this->setUp();
+
+			$this->assertTrue($this->category_status);		
+			$this->assertTrue($this->attribute_status);	
 			
-		$this->assertNotFalse($catat->add_maps($_maps));		
-
-		$carat= new EO_WBC_CatAt();
-		$carat->create_products($carat->product);
-
-		if(!empty($carat->product)){
-
-			$first_product_name = $carat->product[0]['title'];
-			$last_product_name = $carat->product[count($carat->product)-1]['title'];
-
-			$this->assertTrue($this->product_exists($first_product_name));
-			$this->assertTrue($this->product_exists($last_product_name));
-		}
+			$this->assertTrue($this->map_status);
+			
+			$this->assertTrue($this->product_status);
 	}
-
-	public function product_exists($name=''){
-		if(!empty($name)){
-			$product = get_page_by_title( $name, OBJECT, 'product' );
-
-			if(!is_wp_error($product) and !empty($product)){
-				if('publish' === get_post_status( $product->ID )){
-					return true;
-				}					
-			}
-		}
-	}
+	
 }
