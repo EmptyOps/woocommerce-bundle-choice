07c8699db492f58b965a2c64d9c40ebb881a9efc
CmdrMahesh <mahesh@emptyops.com>
2020-01-28 08:52:45 +0530

    test case update

--- tests/import_data_init.php
+++ tests/import_data_init.php
@@ -1,20 +1,4 @@
 <?php
-/*
-* Boilerplate - DO NOT TOUCH ANY LINES HERE.
-* FUTURE : TO BE MOVED TO COMMON FILE.
-*/
-$wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
-require_once $wp_tests_dir . '/includes/functions.php';
-require_once $wp_tests_dir . '/includes/bootstrap.php';
-require_once $wp_tests_dir . '/includes/listener-loader.php';
-
-require_once ABSPATH . 'wp-content/plugins/woocommerce/woocommerce.php';
-require_once dirname( dirname( __FILE__ ) ) . '/woo-bundle-choice.php';		
-
-activate_plugin('woocommerce/woocommerce.php');
-activate_plugin('woocommerce-bundle-choice/woo-bundle-choice.php');		
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
 
 require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
 require_once('data/sample_data.php');
@@ -30,16 +14,14 @@ WC()->initialize_cart();
 include_once WC_ABSPATH . 'includes/class-wc-product-factory.php';		
 WC()->product_factory = new WC_Product_Factory();
 
-
 global $_category;
 global $_atttriutes;
 global $_maps;
 global $_product;
 global $_img_url;
 
-do_action('woocommerce_init');   	
-
-require_once(constant('EO_WBC_PLUGIN_DIR').'EO_WBC_Admin/EO_WBC_Config/EO_WBC_View/library/EO_WBC_CatAt.php');
+global $wp_query;
+global $post;
 
 $factory_object = new EO_WBC_CatAt();
 
@@ -60,8 +42,6 @@ update_option('eo_wbc_config_category',1);
 update_option('eo_wbc_config_map',1);    
 update_option('eo_wbc_btn_setting','0');
 update_option('eo_wbc_btn_position','begining');
-    
-
 
 $wp_query->queried_object = get_term_by( 'slug', get_option('eo_wbc_first_slug'), 'product_cat');
 
@@ -78,6 +58,3 @@ $variation_id = $variable_product->get_available_variations()[0]['variation_id']
 	$sets['SECOND'] = array($post->ID,1,$variation_id);
 
 	WC()->session->set('EO_WBC_SETS',$sets);
-
-WC()->session->get('EO_WBC_SETS',false);
-WC()->session->get('EO_WBC_MAPS',false);
