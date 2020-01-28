960e67e6204ca3fd4ab1ff1fb1d59b36fda15a01
CmdrMahesh <mahesh@emptyops.com>
2020-01-28 09:10:33 +0530

    test case update

--- tests/test-frontend-category.php
+++ tests/test-frontend-category.php
@@ -38,9 +38,6 @@ class FrontendCategory extends WP_UnitTestCase {
 		$eo_wbc_add_to_cart_link = $LoadEO_WBC_Category->eo_wbc_add_to_cart_link();
 		$this->assertNotFalse($eo_wbc_add_to_cart_link);
 		$this->assertNotNull($eo_wbc_add_to_cart_link);
-
-		$LoadEO_WBC_Category->eo_wbc_add_filters();
-		$this->assertTrue( hsa_action('woocommerce_before_shop_loop','function()'));
 				
 		$eo_wbc_prev_url = $LoadEO_WBC_Category->eo_wbc_prev_url();
 		$this->assertNotFalse($eo_wbc_prev_url);
