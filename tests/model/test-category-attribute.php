e506612aef8d9eabc6dba2af108eaf36e60e835a
ajay.bodar <ajay@emptyops.com>
2020-05-15 17:28:45 +0530

    fixed error for test script

--- tests/model/test-category-attribute.php
+++ tests/model/test-category-attribute.php
@@ -3,6 +3,6 @@ class Model_Category_Attribute_Test extends WP_UnitTestCase {
 	function test_get_category() {
 		$result = eo\wbc\model\Category_Attribute::instance()->get_category();
 		//$this->assertNotEmpty($result);
-		$this->assertIsArray($result);
+		$this->assertEquals( is_array($result), true );
 	}	
 }
