40c744e525e5d2862cbd449de1e67eeb1bb909c8
ajay.bodar <ajay@emptyops.com>
2020-05-19 15:38:27 +0530

    appearance module

--- application/helper/helper-WBC_Common.php
+++ application/helper/helper-WBC_Common.php
@@ -37,7 +37,7 @@ class WBC_Common {
 
 	}
 
-	public function consistsOfTheSameValues(array $a, array $b) {
+	public function consistsOfTheSameValues(array $a, array $b, bool $strict = false) {
 	    // check size of both arrays
 	    if (count($a) !== count($b)) {
 	        return false;
@@ -46,12 +46,12 @@ class WBC_Common {
 	    foreach ($b as $key => $bValue) {
 
 	        // check that expected value exists in the array
-	        if (!in_array($bValue, $a, true)) {
+	        if (!in_array($bValue, $a, $strict)) {
 	            return false;
 	        }
 
 	        // check that expected value occurs the same amount of times in both arrays
-	        if (count(array_keys($a, $bValue, true)) !== count(array_keys($b, $bValue, true))) {
+	        if (count(array_keys($a, $bValue, $strict)) !== count(array_keys($b, $bValue, $strict))) {
 	            return false;
 	        }
 
