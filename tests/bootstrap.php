8336b4e5f537eb3d8ef0e40c8d13f638a1ae6421
ajay.bodar <ajay@emptyops.com>
2020-06-11 02:16:01 +0530

    front end modules testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-/**
- * PHPUnit bootstrap file
- *
- * @package Woocommerce_Bundle_Choice_Design_Pattern
- */
+// /**
+//  * PHPUnit bootstrap file
+//  *
+//  * @package Woocommerce_Bundle_Choice_Design_Pattern
+//  */
 
 // $_tests_dir = getenv( 'WP_TESTS_DIR' );
 // $_wp_dir = getenv('WP_CORE_DIR');
@@ -20,16 +20,16 @@
 // // Give access to tests_add_filter() function.
 // require_once $_tests_dir . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
+// /**
+//  * Manually load the plugin being tested.
+//  */
+// function _manually_load_plugin() {
 
-	// require_once '/tmp/wordpress/wp-content/plugins/woocommerce/woocommerce.php';
-	require_once '/tmp/wordpress/src/wp-content/plugins/woocommerce/woocommerce.php';
-	require dirname( dirname( __FILE__ ) ) . '/woocommerce-bundle-choice.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+// 	// require_once '/tmp/wordpress/wp-content/plugins/woocommerce/woocommerce.php';
+// 	require_once '/tmp/wordpress/src/wp-content/plugins/woocommerce/woocommerce.php';
+// 	require dirname( dirname( __FILE__ ) ) . '/woocommerce-bundle-choice.php';
+// }
+// tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // // Start up the WP testing environment.
 // require $_tests_dir . '/includes/bootstrap.php';
