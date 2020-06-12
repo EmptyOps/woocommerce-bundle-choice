c7ec2c3425c2e5b6848881a5a0805edf3af57ee3
ajay.bodar <ajay@emptyops.com>
2020-06-12 15:30:58 +0530

    front end modules testing

--- tests/e2e-tests/checkIfDivExists.js
+++
@@ -1,8 +0,0 @@
-describe('getDiv', function() {
-	window.location.href="/";
-    var d = document.querySelector('DIV');
-
-    it('Should exist', function() {
-        expect(d.nodeName).toBe('DIV');
-    });
-});
