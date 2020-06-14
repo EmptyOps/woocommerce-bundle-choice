75f2e2d2c65f70e27295a40adbfc461d44a3fbaf
ajay.bodar <ajay@emptyops.com>
2020-06-14 14:26:44 +0530

    front end modules testing

--- tests/acceptance/basicCept.php
+++ tests/acceptance/basicCept.php
@@ -9,7 +9,10 @@
 
 $I = new AcceptanceTester( $scenario );
 $I->wantTo( 'Visit WordPress Dashboard' );
+
+//important: loginAsAdmin function will require wp-browser module enabled in codeception yml settings
 // $I->loginAsAdmin();
+
 // $I->see( 'Dashboard' );
 // // echo $I->grabPageSource();
 
