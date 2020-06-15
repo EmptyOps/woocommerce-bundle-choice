ef1fe0f93d826a0ba4ca5c0687a1b20bf9c88db4
ajay.bodar <ajay@emptyops.com>
2020-06-15 16:56:42 +0530

    front end modules testing

--- tests/acceptance/adminConfigurationCest.php
+++ tests/acceptance/adminConfigurationCest.php
@@ -11,55 +11,81 @@ class adminConfigurationCest
     // {
     // }
 
-	public function testSaveOptions(AcceptanceTester $I) {
+    public function configureButtons(AcceptanceTester $I) {
 
-		//login to admin panel 
+		//login to admin panel, should save and maintain cookies so that do not need to login on all admin test. but yeah however during the front end test should flush the admin cookie first.  
 		$I->loginAsAdmin();
 		$I->see( 'Dashboard' );
 
+		// go to the page
 		$I->amOnPage('/wp-admin/admin.php?page=eowbc');
 		// $I->fillField('username', 'davert');
 		// $I->fillField('password', 'qwerty');
 		echo $I->grabPageSource();
-		$I->click('Configuration');
-		return;
-		$I->see('Welcome, Davert!');
+		
+		// go to the tab
+		$I->click('General');
+		$I->click('Buttons');
+		$I->see('Choose where you want to display buttons on home page');
 
-		$_POST['_wpnonce'] = wp_create_nonce('eowbc_configuration');
-		$_POST['resolver'] = 'eowbc_configuration';
-		$_POST['config_business_type'] = 'jewelery';
-		$_POST['config_buttons_page'] = '1';
-		$_POST['config_enable_make_pair'] = 'enable_make_pair';
-		$_POST['config_label_make_pair'] = 'Make Pair';
-		$_POST['config_first_name'] = 'first_name';
-		$_POST['config_first_icon'] = 'first_icon';
-		$_POST['config_second_name'] = 'second_name';
-		$_POST['config_second_icon'] = 'second_icon';
-		$_POST['config_preview_name'] = 'preview_name';
-		$_POST['config_preview_icon'] = 'preview_icon';
-		$_POST['config_filter_status'] = 'filter_status';
-		$_POST['config_pair_maker_status'] = 'pair_maker_status';
-		$_POST['config_pair_maker_upper_card'] = 'pair_maker_upper_card';
+		// select button position
 
-		$expected = serialize(array(
-			"business_type"=>"jewelery",
-			"buttons_page"=>"1",
-			"enable_make_pair"=>"enable_make_pair",
-			"label_make_pair"=>"Make Pair",
-			"first_name"=>"first_name",
-			"first_icon"=>"first_icon",
-			"second_name"=>"second_name",
-			"second_icon"=>"second_icon",
-			"preview_name"=>"preview_name",
-			"preview_icon"=>"preview_icon",
-			"filter_status"=>"filter_status",
-			"pair_maker_status"=>"pair_maker_status",
-			"pair_maker_upper_card"=>"pair_maker_upper_card"			
-		));
 
-		require_once constant('EOWBC_DIRECTORY').'application/controllers/ajax/'.sanitize_text_field($_POST['resolver']).'.php';
-		$result = get_option('eowbc_option_configuration',"a:0:{}");
-		$this->assertEquals($expected , $result);
+		// save 
+
 	}
+
+	// public function testSaveOptions(AcceptanceTester $I) {
+
+	// 	//login to admin panel, should save and maintain cookies so that do not need to login on all admin test. but yeah however during the front end test should flush the admin cookie first.  
+	// 	$I->loginAsAdmin();
+	// 	$I->see( 'Dashboard' );
+
+	// 	$I->amOnPage('/wp-admin/admin.php?page=eowbc');
+	// 	// $I->fillField('username', 'davert');
+	// 	// $I->fillField('password', 'qwerty');
+	// 	echo $I->grabPageSource();
+	// 	$I->click('General');
+	// 	$I->click('Buttons');
+	// 	$I->
+	// 	return;
+	// 	$I->see('Welcome, Davert!');
+
+	// 	$_POST['_wpnonce'] = wp_create_nonce('eowbc_configuration');
+	// 	$_POST['resolver'] = 'eowbc_configuration';
+	// 	$_POST['config_business_type'] = 'jewelery';
+	// 	$_POST['config_buttons_page'] = '1';
+	// 	$_POST['config_enable_make_pair'] = 'enable_make_pair';
+	// 	$_POST['config_label_make_pair'] = 'Make Pair';
+	// 	$_POST['config_first_name'] = 'first_name';
+	// 	$_POST['config_first_icon'] = 'first_icon';
+	// 	$_POST['config_second_name'] = 'second_name';
+	// 	$_POST['config_second_icon'] = 'second_icon';
+	// 	$_POST['config_preview_name'] = 'preview_name';
+	// 	$_POST['config_preview_icon'] = 'preview_icon';
+	// 	$_POST['config_filter_status'] = 'filter_status';
+	// 	$_POST['config_pair_maker_status'] = 'pair_maker_status';
+	// 	$_POST['config_pair_maker_upper_card'] = 'pair_maker_upper_card';
+
+	// 	$expected = serialize(array(
+	// 		"business_type"=>"jewelery",
+	// 		"buttons_page"=>"1",
+	// 		"enable_make_pair"=>"enable_make_pair",
+	// 		"label_make_pair"=>"Make Pair",
+	// 		"first_name"=>"first_name",
+	// 		"first_icon"=>"first_icon",
+	// 		"second_name"=>"second_name",
+	// 		"second_icon"=>"second_icon",
+	// 		"preview_name"=>"preview_name",
+	// 		"preview_icon"=>"preview_icon",
+	// 		"filter_status"=>"filter_status",
+	// 		"pair_maker_status"=>"pair_maker_status",
+	// 		"pair_maker_upper_card"=>"pair_maker_upper_card"			
+	// 	));
+
+	// 	require_once constant('EOWBC_DIRECTORY').'application/controllers/ajax/'.sanitize_text_field($_POST['resolver']).'.php';
+	// 	$result = get_option('eowbc_option_configuration',"a:0:{}");
+	// 	$this->assertEquals($expected , $result);
+	// }
 
 }
