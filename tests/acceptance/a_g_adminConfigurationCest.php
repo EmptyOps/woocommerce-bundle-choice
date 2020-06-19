f1356899dd18eac146c34b91702b9bc82376cf7a
ajay.bodar <ajay@emptyops.com>
2020-06-19 17:47:21 +0530

    front end modules testing

--- tests/acceptance/a_g_adminConfigurationCest.php
+++ tests/acceptance/a_g_adminConfigurationCest.php
@@ -68,16 +68,13 @@ class a_g_adminConfigurationCest
 
 		// save 
 		$I->executeJS('window.scrollTo( 0, jQuery("#config_navigation_conf_save_btn").scrollTop() + 1000 );');		//$I->scrollTo('Save');	//$I->scrollTo('#config_navigation_conf_save_btn');
-		$I->executeJS('jQuery("#config_navigation_conf_save_btn").html("config_navigation_conf_save_btn");');
 		$I->wait(3);
-		echo $I->grabPageSource();
-		print_r( $I->findElementsUsingXPath('#config_navigation_conf_save_btn') );
-		$I->click('config_navigation_conf_save_btn');		//it shouldn't be this way, but there seem some issue with selenium driver and thus when there is another Save button on the page even though on another page and is not visible but still selenium think it is visible and thus gives us error so need to use unique xPath like id etc. 
+		$I->click('Save');		//it shouldn't be this way, but there seem some issue with selenium driver and thus when there is another Save button on the page even though on another page and is not visible but still selenium think it is visible and thus gives us error so need to use unique xPath like id etc. 
 
 		// confirm if saved properly or not
 		$I->amOnPage('/wp-admin/admin.php?page=eowbc-configuration');	//reload page
 		$I->click('Navigations Steps( Breadcrumb )');
-		$I->see('Home page only ???');	//that is the position option selected
+		$I->see('Uncategorized');	//that is the position option selected
 
 	}
 
