da9a7dfe94d42ae7142b1e41689db49fd9fa1243
ajay.bodar <ajay@emptyops.com>
2020-06-20 18:36:34 +0530

    front modules testing

--- tests/acceptance/f_e_homeCest.php
+++ tests/acceptance/f_e_homeCest.php
@@ -11,18 +11,37 @@ class f_e_homeCest
     // {
     // }
 
-    public function testChoiceButtons(AcceptanceTester $I) {
+    public function testChoiceButtonWidgetOnHome(AcceptanceTester $I) {
 		
     	// go to the home page
 		$I->amOnPage('/');
 
 		// Check if buttons with text x are visible 
+        $I->see('Start with Diamond');
 
+		// I click on button one and I see in next page text like 1 {button text}
+        $I->click('Start with Diamond');
+        $I->waitForText('1 Choose Diamond', 10);
 
-		// I click on button one and I see in next page text like 1 button text
+        //go back to the home page
+        $I->moveBack();
 
-		// I click on button two  and I see in next page text like 1 button text of second Button
+		// I click on button two  and I see in next page text like 1 {button text} of second Button
+        $I->click('Start with Setting');
+        $I->waitForText('1 Choose Setting', 10);
 
 	}
+
+    public function testChoiceButtonWidgetOnLandingPage(AcceptanceTester $I) {
+        
+        // TODO
+
+    }
+
+    public function testChoiceButtonWidgetBasedOnShortcode(AcceptanceTester $I) {
+        
+        // TODO
+
+    }
 
 }
