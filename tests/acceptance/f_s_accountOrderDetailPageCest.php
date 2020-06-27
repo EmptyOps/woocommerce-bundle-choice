1f0d14063cbe8025a5b4557d0890a4b1cbcabc98
ajay.bodar <ajay@emptyops.com>
2020-06-27 14:22:30 +0530

    frontend modules tests

--- tests/acceptance/f_s_accountOrderDetailPageCest.php
+++ tests/acceptance/f_s_accountOrderDetailPageCest.php
@@ -6,8 +6,72 @@ class f_s_accountOrderDetailPageCest
     {
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
-    }
+    // // tests
+    // public function tryToTest(AcceptanceTester $I)
+    // {
+    // }
+
+    function myAccountPage(AcceptanceTester $I) {
+		
+		//here it is better if we can simply go to my account page via login or even better if WebDriver can keep the previous session alive 
+		if( false ) {
+	    	// go to the preview page of step 3
+	    	require_once 'f_q_checkoutPageCest.php';
+	    	$f_q_checkoutPageCestObj = new f_q_checkoutPageCest();
+	    	$f_q_checkoutPageCestObj->continueToCheckout($I,true);
+
+	    	// verify 
+	    	$I->waitForText('Billing details', 10, 'h3');
+			$I->waitForText('Have a coupon?', 10, 'div');
+			$I->waitForText('Place order', 10, 'button');
+		}
+		else {
+
+			//here if session is not detected than try to keep the previous session alive and it must be possible in WebDriver 
+			$I->amOnPage('/my-account/orders/');	
+			$I->click('VIEW', 'a');	
+
+		}
+
+	}
+
+	public function verifyOrderDetails(AcceptanceTester $I, $is_inner_call=false) {
+
+		$this->myAccountPage($I);
+
+		// verify order details 
+		$I->waitForText('Order details', 10, 'h2');
+		$I->waitForText('hi000', 10, 'address');
+		$I->waitForText('Direct bank transfer', 10, 'td');
+		$I->waitForText('08347408752', 10, 'p');
+
+		//TODO check here if merged row appears properly or not 
+		if( !$is_inner_call ) { 
+			$this->confirmMergedRow($I);	
+		}
+
+	}
+
+	function confirmMergedRow(AcceptanceTester $I) {
+		
+		// // - I choose filter options and then I check if x  products are found
+		// // - I click on product image of first product from the search results
+		// $price_of_product = $this->categoryPage($I);
+
+		// // - I see continue button
+		// $I->see('Add to bag...');
+
+		// // with text x ???
+		// $I->see($price_of_product, 'span');
+
+		// // - I click on continue button
+		// $I->click('Add to bag...');
+
+		// // - I see in next page the text "${price of Step 1 item's price}"
+		// $I->waitForText($price_of_product, 10, 'span');
+		// $I->waitForText('VIEW', 10, 'a');
+		// $I->waitForText('REMOVE', 10, 'a');
+
+	}
+
 }
---
+++ tests/acceptance/n_e_installAndSetupAdminSideCest.php
@@ -0,0 +1,50 @@
+<?php 
+
+//class intended to do a normal scenario test in a way in which normally users would try the plugin
+class n_e_installAndSetupAdminSideCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // install
+    public function tryToTestInstall(AcceptanceTester $I)
+    {
+    }
+
+    // setup using setup wizard and sample data. 
+    // TODO here important to note that to run test with sample data on fresh install this test either needs to be run first or coditionally in duplicate environment where the other tests which sets data manually are just skipped  
+    public function tryToTestSetupUsingSampleData(AcceptanceTester $I)
+    {
+    	return;	//TODO run this test only if sample data env flag is set 
+
+    	//login to admin panel, should save and maintain cookies so that do not need to login on all admin test. but yeah however during the front end test should flush the admin cookie first.  
+		$I->loginAsAdmin();
+		$I->see( 'Dashboard' );
+
+		// go to the page
+		$I->amOnPage('/wp-admin/admin.php?page=eowbc-configuration');
+		
+		// verify the tab
+		$I->see('This section will help you add sample data and configurations');
+
+		// start sample data insert process  
+		$I->click('Click here for automated configuration and setup');
+
+		// confirm if saved properly or not
+		$I->reloadPage();	//reload page
+		$I->click('Buttons');
+		$I->see('Custom landing page');	//that is the position option selected
+    }
+
+    // try alternate widgets 
+    public function tryToTestAlternateBreadcrumbWidgets(AcceptanceTester $I)
+    {
+    }
+
+	// try alternate widgets 
+    public function tryToTestAlternateFilterWidgets(AcceptanceTester $I)
+    {
+    }
+
+}
