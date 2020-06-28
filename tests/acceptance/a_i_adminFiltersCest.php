2a87df8bbb06ed4a830da04d1e885bb5c559f019
ajay.bodar <ajay@emptyops.com>
2020-06-28 18:23:47 +0530

    front modules tests

--- tests/acceptance/a_i_adminFiltersCest.php
+++ tests/acceptance/a_i_adminFiltersCest.php
@@ -48,13 +48,13 @@ class a_i_adminFiltersCest
 
 		// go to the tab
 		$I->click('Alternate Filter Widgets');
-		$I->see('First Category');
+		$I->see('Default(Grid View)');
 
 		// select category
 		$I->executeJS("jQuery('#sc3').checkbox('set checked');");	
 
 		// save 
-		$I->click('Save');		//click('#filter_setting_submit_btn'); 	//('Save');		//it shouldn't be this way, but there seem some issue with selenium driver and thus when there is another Save button on the page even though on another page and is not visible but still selenium think it is visible and thus gives us error so need to use unique xPath like id etc. 
+		$I->click('#submit_btn'); 	//('Save');		//it shouldn't be this way, but there seem some issue with selenium driver and thus when there is another Save button on the page even though on another page and is not visible but still selenium think it is visible and thus gives us error so need to use unique xPath like id etc. 
 
 		// confirm if saved properly or not
 		$I->reloadPage();	//reload page
