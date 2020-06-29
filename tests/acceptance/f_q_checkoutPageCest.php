3f8ff29dd8701f1c007d3883e9b063462c27dcae
ajay.bodar <ajay@emptyops.com>
2020-06-29 18:04:32 +0530

    front modules tests

--- tests/acceptance/f_q_checkoutPageCest.php
+++ tests/acceptance/f_q_checkoutPageCest.php
@@ -11,28 +11,25 @@ class f_q_checkoutPageCest
     // {
     // }
 
-	function checkoutPage(AcceptanceTester $I) {
+	public function checkoutPage(AcceptanceTester $I, $is_inner_call=false) {
 		
-    	// go to the preview page of step 3
-    	require_once dirname(__FILE__).'f_o_cartPageCest.php';
-    	$f_o_cartPageCestObj = new f_o_cartPageCest();
-    	$f_o_cartPageCestObj->continueToCheckout($I,true);
+		//TODO make it dynamic by saving this in session in previous steps and then here get it from session 
+    	$this->price_of_product_step1 = "12.00";
+    	$this->price_of_product_step2 = "15.00";
 
     	// verify 
-    	$I->see('Billing details', 10, 'h3');
-		$I->see('Have a coupon?', 10, 'div');
-		$I->see('Place order', 10, 'button');
-
-	}
-
-	public function placeOrder(AcceptanceTester $I, $is_inner_call=false) {
-
-		$this->checkoutPage($I);
-
+    	$I->see('Billing details');
+		$I->see('Have a coupon?');
+		$I->see('Place order');
+		
 		//TODO check here if merged row appears properly or not
 		if( !$is_inner_call ) { 
 			$this->confirmOrderSummary($I);	
 		}
+
+	}
+
+	public function placeOrder(AcceptanceTester $I, $is_inner_call=false) {
 
 		// setup fields 
 		$I->fillField("billing_first_name", 'hi000');
@@ -52,31 +49,18 @@ class f_q_checkoutPageCest
 
 		// verify if order placed properly on the thank you page
 		$I->waitForText('Order received', 10, 'h1');
-		$I->waitForText('sales@sphereplugins.com', 10, 'strong');
-		$I->waitForText('Direct bank transfer', 10, 'strong');
-		$I->waitForText('8347408752', 10, 'p');
+		$I->see('sales@sphereplugins.com');
+		$I->see('Direct bank transfer', 'strong');
+		$I->see('8347408752');
 
 	}
 
-	function confirmOrderSummary(AcceptanceTester $I) {
+	protected function confirmOrderSummary(AcceptanceTester $I) {
 		
-		// // - I choose filter options and then I check if x  products are found
-		// // - I click on product image of first product from the search results
-		// $price_of_product = $this->categoryPage($I);
+		// TODO implement complete check which ensures in single row UI widget the entire item pair is displayed and not in two 
 
-		// // - I see continue button
-		// $I->see('Add to bag...');
-
-		// // with text x ???
-		// $I->see($price_of_product, 'span');
-
-		// // - I click on continue button
-		// $I->click('Add to bag...');
-
-		// // - I see in next page the text "${price of Step 1 item's price}"
-		// $I->waitForText($price_of_product, 10, 'span');
-		// $I->waitForText('VIEW', 10, 'a');
-		// $I->waitForText('REMOVE', 10, 'a');
+		// at least price check is done but however even for this it needs to confirm that it check the order summary row price and not of any subtotal or total. 
+		$I->see($this->price_of_product_step1+$this->price_of_product_step2);	
 
 	}
 
--- tests/acceptance/f_s_accountOrderDetailPageCest.php
+++ tests/acceptance/f_s_accountOrderDetailPageCest.php
@@ -11,39 +11,32 @@ class f_s_accountOrderDetailPageCest
     // {
     // }
 
-    function myAccountPage(AcceptanceTester $I) {
+    public function myAccountPage(AcceptanceTester $I) {
+
+    	//TODO make it dynamic by saving this in session in previous steps and then here get it from session 
+    	$this->price_of_product_step1 = "12.00";
+    	$this->price_of_product_step2 = "15.00";
 		
-		//here it is better if we can simply go to my account page via login or even better if WebDriver can keep the previous session alive 
-		if( false ) {
-	    	// go to the preview page of step 3
-	    	require_once dirname(__FILE__).'f_q_checkoutPageCest.php';
-	    	$f_q_checkoutPageCestObj = new f_q_checkoutPageCest();
-	    	$f_q_checkoutPageCestObj->continueToCheckout($I,true);
-
-	    	// verify 
-	    	$I->waitForText('Billing details', 10, 'h3');
-			$I->waitForText('Have a coupon?', 10, 'div');
-			$I->waitForText('Place order', 10, 'button');
-		}
-		else {
-
-			//here if session is not detected than try to keep the previous session alive and it must be possible in WebDriver 
-			$I->amOnPage('/my-account/orders/');	
-			$I->click('VIEW', 'a');	
-
-		}
-
+		//here if session is not detected than try to keep the previous session alive and it must be possible in WebDriver 
+		$I->amOnPage('/my-account/orders/');	
+		
+		// verify 
+    	$I->see('Billing details');
+		$I->see('Have a coupon?');
+		$I->see('Place order');
+		
 	}
 
 	public function verifyOrderDetails(AcceptanceTester $I, $is_inner_call=false) {
 
-		$this->myAccountPage($I);
+		// go to detail page 
+		$I->click('VIEW', 'a');	
 
 		// verify order details 
 		$I->waitForText('Order details', 10, 'h2');
-		$I->waitForText('hi000', 10, 'address');
-		$I->waitForText('Direct bank transfer', 10, 'td');
-		$I->waitForText('08347408752', 10, 'p');
+		$I->see('hi000');
+		$I->see('Direct bank transfer');
+		$I->see('08347408752');
 
 		//TODO check here if merged row appears properly or not 
 		if( !$is_inner_call ) { 
@@ -54,23 +47,10 @@ class f_s_accountOrderDetailPageCest
 
 	function confirmMergedRow(AcceptanceTester $I) {
 		
-		// // - I choose filter options and then I check if x  products are found
-		// // - I click on product image of first product from the search results
-		// $price_of_product = $this->categoryPage($I);
+		// TODO implement complete check which ensures in single row UI widget the entire item pair is displayed and not in two 
 
-		// // - I see continue button
-		// $I->see('Add to bag...');
-
-		// // with text x ???
-		// $I->see($price_of_product, 'span');
-
-		// // - I click on continue button
-		// $I->click('Add to bag...');
-
-		// // - I see in next page the text "${price of Step 1 item's price}"
-		// $I->waitForText($price_of_product, 10, 'span');
-		// $I->waitForText('VIEW', 10, 'a');
-		// $I->waitForText('REMOVE', 10, 'a');
+		// at least price check is done but however even for this it needs to confirm that it is checking the row price and not of any subtotal or total. 
+		$I->see($this->price_of_product_step1+$this->price_of_product_step2);
 
 	}
 
