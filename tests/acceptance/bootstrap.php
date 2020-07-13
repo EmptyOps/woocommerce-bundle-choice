e66dd0508d5ceb3a46f20507ae3da0c8bc7105b7
ajay.bodar <ajay@emptyops.com>
2020-07-14 02:01:05 +0530

    front modules tests

---
+++ tests/acceptance/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bootstrap file for the acceptance tests.
+ *
+ * Required by Codeception, but currently unused.
+ *
+ * @package wp-browser-travis-demo
+ * @since   1.0.0
+ */
+
+echo "bootstrap outer called...";
+// throw new Exception("bootstrap outer called...", 1);
+
+// EOF
