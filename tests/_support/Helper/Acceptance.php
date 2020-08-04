8df65c418c098b39652d4f1925b22ceef8c49785
ajay.bodar <ajay@emptyops.com>
2020-08-05 01:07:35 +0530

    frontend modules tests

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -411,21 +411,10 @@ class Acceptance extends \Codeception\Module
         // echo "called resetSession...";
             
         try { 
-            $I->executeJS('
-                var hexDigits = new Array
-                        ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+            // $I->executeJS('
+                
 
-                //Function to convert rgb color to hex format
-                function rgb2hex(rgb) {
-                 rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-                 return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-                }
-
-                function hex(x) {
-                  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-                 }
-
-                ');
+            //     ');
             return true;
         }
         catch(Exception $e) {
@@ -444,7 +433,22 @@ class Acceptance extends \Codeception\Module
         // echo "called resetSession...";
             
         try { 
-            return $I->executeJS('return rgb2hex( jQuery("'.$selector_of_targets.'").css("'.$css_property_of_targets.'") );');  
+            return $I->executeJS('
+                                var hexDigits = new Array
+                                        ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+
+                                //Function to convert rgb color to hex format
+                                function rgb2hex(rgb) {
+                                 rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+                                 return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+                                }
+
+                                function hex(x) {
+                                  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+                                 }
+
+                                return rgb2hex( jQuery("'.$selector_of_targets.'").css("'.$css_property_of_targets.'") );
+                                ');  
         }
         catch(Exception $e) {
             echo "caught message...";
