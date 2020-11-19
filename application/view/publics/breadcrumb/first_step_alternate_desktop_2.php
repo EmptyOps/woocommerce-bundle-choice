1bf8965838c1c22c18f02c0ee5123cb08c292cf9
CmdrMahesh <mahesh@emptyops.com>
2020-11-19 16:41:04 +0530

    commit@mahesh 19-11-2020 v2

--- application/view/publics/breadcrumb/first_step_alternate_desktop_2.php
+++ application/view/publics/breadcrumb/first_step_alternate_desktop_2.php
@@ -14,7 +14,7 @@
                 <div class="title" <?php _e((!empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$clickable_breadcrumb) and !empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$first_url))?'data-clickable_breadcrumb="'.\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$first_url.'"':''); ?>><?php _e(wbc()->options->get_option('appearance_breadcrumb','appearance_breadcrumb_choose_prefix_text',__('Choose a','woo-bundle-choice'),true,true)); ?> <?php _e($first_name); ?></div>
             <?php } else { ?>
                 <div class="title"><?php _e($first_name); ?></div>
-                <div class="description"><?php _e($first->get_name()); ?> - <?php _e(wc_price($first->get_price())); ?></div>
+                <div class="description"><?php _e(/*$first->get_name()*/$first->get_title()); ?> - <?php _e(wc_price($first->get_price())); ?></div>
                 <div class="ui small hover green text">                
                     <u><a href="<?php echo !empty(wbc()->sanitize->get('FIRST')) ? eo\wbc\model\publics\component\EOWBC_Breadcrumb::eo_wbc_breadcrumb_view_url(wbc()->sanitize->get('FIRST'),$order):'#'; ?>">View</a></u>&nbsp;|&nbsp;<u><a href="<?php echo !empty(wbc()->sanitize->get('FIRST'))?eo\wbc\model\publics\component\EOWBC_Breadcrumb::eo_wbc_breadcrumb_change_url($order,wbc()->sanitize->get('FIRST')):'#'; ?>"><?php _e(wbc()->options->get_option('appearance_breadcrumb','appearance_breadcrumb_change_action_text','Change',true,true)); ?></a></u>
                 </div>    
@@ -24,8 +24,15 @@
         <div class="ui column mini image left aligned" style="padding-top: 0px;padding-bottom: 0px;">
             <?php if(empty($first)){ ?>
                 <img src = '<?php echo $first_icon; ?>' class='ui mini image'/>
-            <?php } else { ?>
-                <img src = '<?php _e(wp_get_attachment_url($first->get_image_id())); ?>' class='ui mini image'/>
+            <?php } else { 
+                    global $woocommerce;
+                    $image = wp_get_attachment_url($first->get_image_id());
+                    if (empty($image)) {
+                        $image = $woocommerce->plugin_url() . '/assets/images/placeholder.png';
+                    }
+
+                ?>
+                <img src = '<?php _e($image); ?>' class='ui mini image'/>
             <?php } ?>
         </div>
     </div>        
--- application/view/publics/breadcrumb/second_step_alternate_desktop_2.php
+++ application/view/publics/breadcrumb/second_step_alternate_desktop_2.php
@@ -14,7 +14,7 @@
                 <div class="title" <?php _e((!empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$clickable_breadcrumb) and !empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$second_url))?'data-clickable_breadcrumb="'.\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$second_url.'"':''); ?>><?php _e(wbc()->options->get_option('appearance_breadcrumb','appearance_breadcrumb_choose_prefix_text',__('Choose a','woo-bundle-choice'),true,true)); ?> <?php _e($second_name); ?></div>
             <?php } else { ?>
                 <div class="title"><?php _e($second_name); ?></div>
-                <div class="description"><?php _e($second->get_name()); ?> - <?php _e(wc_price($second->get_price())); ?></div>
+                <div class="description"><?php _e(/*$second->get_name()*/$second->get_title()); ?> - <?php _e(wc_price($second->get_price())); ?></div>
                 <div class="ui small blue text">                
                     <u><a href="<?php echo !empty(wbc()->sanitize->get('SECOND')) ? eo\wbc\model\publics\component\EOWBC_Breadcrumb::eo_wbc_breadcrumb_view_url(wbc()->sanitize->get('SECOND'),$order):'#'; ?>">View</a></u>&nbsp;|&nbsp;<u><a href="<?php echo !empty(wbc()->sanitize->get('SECOND'))?eo\wbc\model\publics\component\EOWBC_Breadcrumb::eo_wbc_breadcrumb_change_url($order,wbc()->sanitize->get('SECOND')):'#'; ?>"><?php _e(wbc()->options->get_option('appearance_breadcrumb','appearance_breadcrumb_change_action_text','Change',true,true)); ?></a></u>
                 </div>    
@@ -24,9 +24,15 @@
         <div class="ui column mini image left aligned" style="padding-top: 0px;padding-bottom: 0px;">
             <?php if(empty($second)){ ?>
                 <img src = '<?php echo $second_icon; ?>' class='ui mini image'/>
-            <?php } else { ?>
+            <?php } else { 
+                    global $woocommerce;
+                    $image = wp_get_attachment_url($second->get_image_id());
+                    if (empty($image)) {
+                        $image = $woocommerce->plugin_url() . '/assets/images/placeholder.png';
+                    }
+                ?>
 
-                <img src = '<?php _e(wp_get_attachment_url($second->get_image_id())); ?>' class='ui mini image'/>
+                <img src = '<?php _e($image); ?>' class='ui mini image'/>
             <?php } ?>
         </div>
     </div>        
