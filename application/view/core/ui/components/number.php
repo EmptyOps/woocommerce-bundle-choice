da0048e6b0d7f7abbff9c545aff0c630abbc2ebc
CmdrMahesh <mahesh@emptyops.com>
2020-12-07 11:39:41 +0530

    commit@mahesh 07-12-2020

--- application/view/core/ui/components/number.php
+++ application/view/core/ui/components/number.php
@@ -4,13 +4,14 @@
 
 <?php if(!empty($name) and !empty($id)): ?>
 
-<input type="text" 
+<input type="number" 
 	name="<?php _e($name); ?>" 
 	id="<?php echo _e($id); ?>"
 	class="<?php !empty($class) ? _e($class) : ''; ?>" 
 	style="<?php !empty($style) ? _e($style) : ''; ?>"
 	<?php isset($min)?' min="'.$min.'" ' : ''; ?>
 	<?php isset($max)?' max="'.$max.'" ' : ''; ?>	
+	<?php isset($attr)?_($attr) : ''; ?>	
 />
 
 <?php endif; ?>
