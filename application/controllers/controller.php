734d32d0697886ee2190e658f1859c9dbb22b2b5
CmdrMahesh <mahesh@emptyops.com>
2020-12-12 20:07:13 +0530

    12-12-2020

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -43,33 +43,50 @@ class Controller {
 	}
 
 	public function generate_appearece($form){
-		if(empty($form)){
+		if(empty($form) or !is_array($form)){
 			return array();
 		}
 
-		wbc()->load->modal('admin/form-elements');
+		wbc()->load->model('admin/form-elements');
 		$admin_controls = array();
 
 		$admin_ui = \eo\wbc\model\admin\Form_Elements::instance();
-
+		
 		foreach ($form as $form_key => $form_value) {
+			
 			if(!empty($form_value['admin_controls'])) {
 				$controls = array();
+				
 				if(!empty($form_value['admin_controls'][1]) and is_array($form_value['admin_controls'])) {
-					$controls = $form_value['admin_controls'];
+					$controls = $form_value['admin_controls'][1];
 				} elseif (!empty($form_value['admin_controls'][1])  and is_string($form_value['admin_controls'])) {
-					$controls = explode(',',$form_value['admin_controls']);
+					$controls = explode(',',$form_value['admin_controls'][1]);
 				}
 
 				if(!empty($controls)){
 					foreach ($controls as $control) {
-						$admin_controls[$form_key.'_'.$control] = call_user_func_array(array($admin_ui,$control),array($form_key.'_'.$control,$form_value['admin_controls'][0]));
+
+						$admin_controls[$form_key.'_'.$control] = 
+
+						call_user_func_array(array($admin_ui,$control),array($form_key.'_'.$control,$form_value['admin_controls'][0]));
 					}
 				}
 			}
 
-			if(!empty($form_value['child'])){
-				$admin_controls = array_unique($admin_controls,$this->generate_appearece($form_value['child']));
+			if(!empty($form_value['child']) or (empty($form_value['type']) and !empty($form_value) and is_array($form_value)) ){
+
+				$child_control = array();
+
+				if(!empty($form_value['child'])){
+
+					$child_control = $this->generate_appearece($form_value['child']);
+				} elseif(empty($form_value['type']) and !empty($form_value) and is_array($form_value)) {
+					$child_control = $this->generate_appearece($form_value);
+				}
+
+				if(!empty($child_control)){
+					$admin_controls = array_replace($admin_controls,$child_control);
+				}
 			}
 		}
 
--- application/model/admin/form-elements.php
+++ application/model/admin/form-elements.php
@@ -25,19 +25,105 @@ class Form_Elements {
 		
 	}
 
-	public function color() {
+	
+	public function border_color($key,$label,$args=array()) {
+		extract($args);
 
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified Border Color on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		} else {
+			$info = array( 'label'=>$info,
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		$args['info'] = $info;
+		$args['label'] = "${label} Border Color";
+
+		return $this->color($key,$label,$args);
+	}
+
+	public function hover_color($key,$label,$args=array()) {
+		extract($args);
+
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified On Hover Color on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		} else {
+			$info = array( 'label'=>$info,
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		$args['info'] = $info;
+		$args['label'] = "${label} On Hover Color";
+
+		return $this->color($key,$label,$args);
+	}
+
+	public function color($key,$label,$args=array()) {
+		extract($args);
+		if(empty($required)) {
+			$required = false;
+		}
+
+		if(empty($default)) {
+			$default = '';
+		}
+
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified Color on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		if(empty($validate)){
+			if($required){
+				$validate = array('required'=>'');
+			} else {
+				$validate = array();
+			}
+		} else {
+			if($required and  array_key_exists('required',$validate)) {
+				$validate['required'] = '';
+			} else {
+				$validate = array();
+			}
+		}
+
+		if(!empty($args['label'])){
+			$label = $args['label'];
+		} else {
+			$label = "${label} Color";
+		}
+
+		return array(
+			'label'=>$label,
+			'type'=>'color',
+			'sanitize'=>'sanitize_hex_color',
+			'validate'=>$validate,
+			'size_class'=>array('eight','wide'),
+			'inline'=>false,
+			'value'=>$default,
+			'visible_info'=>$info
+		);
 	}	
 
 	public function select() {
 
 	}
 
-	public function radius() {
+	
 
-	}
-
-	public function text($key,$label,$args) {
+	public function text($key,$label,$args=array()) {
 		
 		extract($args);
 		if(empty($required)) {
@@ -53,25 +139,30 @@ class Form_Elements {
 				'type'=>'visible_info',
 				'class'=>array('small'),				
 			);
-		} else {
-			$info = array( 'label'=>$info,
-				'type'=>'visible_info',
-				'class'=>array('small'),				
-			);
 		}
 
 		if(empty($validate)){
 			if($required){
 				$validate = array('required'=>'');
+			} else {
+				$validate = array();
 			}
 		} else {
 			if($required and  array_key_exists('required',$validate)) {
 				$validate['required'] = '';
+			} else {
+				$validate = array();
 			}
 		}
 
-		array(
-			'label'=>"${label} Text",
+		if(!empty($args['label'])){
+			$label = $args['label'];
+		} else {
+			$label = "${label} Text";
+		}
+
+		return array(
+			'label'=>$label,
 			'type'=>'text',
 			'sanitize'=>'sanitize_text_field',
 			'validate'=>$validate,
@@ -82,11 +173,70 @@ class Form_Elements {
 		);
 	}
 
-	public function font_family($key) {		
-		return $this->label($key);
+	public function font($key,$label,$args=array()){
+		return $this->font_family($key,$label,$args);
+	}
+	public function font_family($key,$label,$args=array()) {
+
+		extract($args);
+
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified font-family on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		} else {
+			$info = array( 'label'=>$info,
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		$args['info'] = $info;
+		$args['label'] = "${label} Font Family";
+
+		return $this->text($key,$label,$args);
 	}
 
-	public function font_size($key) {
-		return $this->label($key);
+	public function font_size($key,$label,$args=array()) {
+
+		extract($args);
+
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified font-size on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		} else {
+			$info = array( 'label'=>$info,
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		$args['info'] = $info;
+		$args['label'] = "${label} Font Size";
+		return $this->text($key,$label,$args);
+	}
+
+	public function radius($key,$label,$args=array()) {
+		extract($args);
+
+		if(empty($info)){
+			$info = array( 'label'=>eowbc_lang("Sets specified border radius on ${label}"),
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		} else {
+			$info = array( 'label'=>$info,
+				'type'=>'visible_info',
+				'class'=>array('small'),				
+			);
+		}
+
+		$args['info'] = $info;
+		$args['label'] = "${label} Border Radius";
+
+		return $this->text($key,$label,$args);
 	}
 }
