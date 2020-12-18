efc64cb32c3fa093d5c04739b72a0eb712d65be7
CmdrMahesh <mahesh@emptyops.com>
2020-12-18 10:41:40 +0530

    commit@mahesh 18-12-2020

--- application/model/publics/component/eowbc_filter_widget.php
+++ application/model/publics/component/eowbc_filter_widget.php
@@ -1620,9 +1620,9 @@ class EOWBC_Filter_Widget {
 
 	public function load_desktop($general_filters, $advance_filters) {
 
-		echo "load_desktop non_adv_ordered_filter and adv_ordered_filter dump";
+/*		echo "load_desktop non_adv_ordered_filter and adv_ordered_filter dump";
 		wbc()->common->pr($general_filters);
-		wbc()->common->pr($advance_filters);
+		wbc()->common->pr($advance_filters);*/
 
 		$category = $this->_category;
 		
@@ -2448,9 +2448,9 @@ class EOWBC_Filter_Widget {
 		ksort($non_adv_ordered_filter);		
 		ksort($adv_ordered_filter);
 
-		echo "non_adv_ordered_filter and adv_ordered_filter dump";
+		/*echo "non_adv_ordered_filter and adv_ordered_filter dump";
 		wbc()->common->pr($non_adv_ordered_filter);
-		wbc()->common->pr($adv_ordered_filter);
+		wbc()->common->pr($adv_ordered_filter);*/
 
 		?>
 		<!--Primary filter button that will only be visible on desktop/tablet-->
@@ -2522,9 +2522,9 @@ class EOWBC_Filter_Widget {
 					)],array_values($non_adv_ordered_filter));			
 		}
 
-		echo "non_adv_ordered_filter and adv_ordered_filter dump 1";
+		/*echo "non_adv_ordered_filter and adv_ordered_filter dump 1";
 		wbc()->common->pr($non_adv_ordered_filter);
-		wbc()->common->pr($adv_ordered_filter);
+		wbc()->common->pr($adv_ordered_filter);*/
 
 		$this->load_filters($non_adv_ordered_filter,$adv_ordered_filter);
 				
