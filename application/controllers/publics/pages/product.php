e8fb13df3265d079a8ac54fdfe4307b1f26afc7e
CmdrMahesh <mahesh@emptyops.com>
2021-04-08 10:13:00 +0530

    commit@mahesh - 08-04-2021 --

--- application/controllers/publics/pages/product.php
+++ application/controllers/publics/pages/product.php
@@ -368,6 +368,7 @@ class Product {
                 if($return_link) {
                     return $url;
                 }
+                
                 return header("Location: {$url}");
                 wp_die();
                 //wp_safe_redirect($url ,301 );               
