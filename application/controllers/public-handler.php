7953d348b21a5697558bd06f997c8d4b8069f70a
CmdrMahesh <mahesh@emptyops.com>
2021-07-09 16:43:47 +0530

    commit@mahesh -- 09-07-2021 --

--- application/controllers/public-handler.php
+++ application/controllers/public-handler.php
@@ -73,19 +73,22 @@ class Public_Handler {
         		$_GET['SECOND']='';
         	}
 
-
-        	if(isset($_GET['EO_WBC']) and !empty($_GET['EO_WBC'])){
-	            // on load filter
-	            $_GET['eo_wbc_filter']=1;
-	            if(!empty($_GET['CAT_LINK'])){
-	                $_GET['_category']='cat_link';
-	                $_REQUEST['_category']='cat_link';
-	                $_GET['cat_filter_cat_link']=$_GET['CAT_LINK'];
-	                $_REQUEST['cat_filter_cat_link']=$_GET['CAT_LINK'];
-	            }        
-	            \eo\wbc\controllers\ajax\Filter::instance()->filter();
-	            unset($_GET['eo_wbc_filter']);
-	        }
+        	add_action('template_redirect',function(){
+        		if (is_product_category()) {
+		        	if(isset($_GET['EO_WBC']) and !empty($_GET['EO_WBC'])){
+			            // on load filter
+			            $_GET['eo_wbc_filter']=1;
+			            if(!empty($_GET['CAT_LINK'])){
+			                $_GET['_category']='cat_link';
+			                $_REQUEST['_category']='cat_link';
+			                $_GET['cat_filter_cat_link']=$_GET['CAT_LINK'];
+			                $_REQUEST['cat_filter_cat_link']=$_GET['CAT_LINK'];
+			            }        
+			            \eo\wbc\controllers\ajax\Filter::instance()->filter();
+			            unset($_GET['eo_wbc_filter']);
+			        }
+			    }
+		    });
 
 
         	add_action('template_redirect',function(){
