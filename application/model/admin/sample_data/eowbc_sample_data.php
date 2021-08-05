a3e3fe7b0513fa43731301885bf867a2816d07f8
CmdrMahesh <mahesh@emptyops.com>
2021-08-05 20:48:54 +0530

    commit@mahesh - 05-08-2021 -- bug fix: eowbc_sample_data unserialize expect second param to be array

--- application/model/admin/sample_data/eowbc_sample_data.php
+++ application/model/admin/sample_data/eowbc_sample_data.php
@@ -81,7 +81,7 @@ class Eowbc_Sample_Data {
 	    
 	        //Send for creation and update returned array.
 
-	        $catat_attribute = unserialize( wbc()->options->get($feature_key.'_created_attribute'), 'a:0:{}' ); 	//$this->create_attribute($attributes);            
+	        $catat_attribute = unserialize(wbc()->options->get($feature_key.'_created_attribute','a:0:{}')); 	//$this->create_attribute($attributes);            
 	        
 	        // update_option('eo_wbc_attr',serialize($catat_attribute));
 	        wbc()->options->set('eo_wbc_attr',serialize($catat_attribute));
