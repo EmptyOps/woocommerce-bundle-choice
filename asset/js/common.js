ef3ec0f13f7ce5ab6ac9a9342a6b04741d7240fb
ajay.bodar <ajay@emptyops.com>
2022-05-22 22:18:53 +0530

    untested

--- asset/js/common.js
+++ asset/js/common.js
@@ -158,3 +158,79 @@ window.document.splugins.Feed.events.core = function() {
 // // Observer 2 is notified!
 // // Observer 3 is notified!
 // // Observer 4 is notified!
+
+// the variations js module
+window.document.splugins.wbc.variations = window.document.splugins.wbc.variations || {};
+
+window.document.splugins.wbc.variations.core = function() {
+    // this.subjects = [];
+
+    this.$wrapper = this._element.closest('.product');
+    this.$variations_form = this.$wrapper.find('.variations_form');
+
+    here mostly in the private scope, the variations module should subscribe to search filter events and pass those to variations core which would call the change event so that filters those affecting the variations data like images etc. are rendered accordingly. so that metal color based or shape based images render appropriately. 
+        --  however, it is not limited to js layer only and actually js layer here would not be of use except the search is client side only based on the js. but the searches are always carried on the backend so the php layer need to ensure that return appropriate variations images etc. whenever the selected options of the search filters connects with variations instead of the main product. 
+            --  m have did it already but need to implement throughly as per standard if not proper yet 
+
+    --  if our slider/zoom module is enabled then 
+        --  simply listen legacy js layer events and on variation change etc. keep updating our dom 
+    --  if our slide/zoom module is not enabled and we are binding to slider/zoom module of the user through the flows of theme adaption template file then 
+        --  simply listen legacy js layer events and then do our applicable logic as well as call the slider/zoom module api that we are binding to 
+    --  if none of the two above then 
+        --  then listen to legacy js layer, do our logic and then publish events on our js layer 
+            --  in this case sample applies for php layer as well as planned, means publishing the php events/data to the php hooks 
+    --  if none of the three above then 
+        --  then user would be using one of our recommend slider and zoom modules out of the 5 recommended plugins we planned to present 
+            --  in this case it will be second if layer above so carry accoding to that layer flows 
+
+    var init = function() {
+        
+        var element = document.getElementById('attribute-to-change');
+        element.innerHTML = contents;
+    }
+
+    return {
+
+        before_search: function() {
+
+            window.document.splugins.variation.events.core.notifyAllObservers( 'variation', 'before_search' ); 
+        }, 
+        // createSubject: function( feature_unique_key, notifications ) {
+        //     // console.log("Observer " + index + " is notified!");
+
+        //     // TODO check if subject already created and exist then throw error
+        //     // var index = this.observers.indexOf(observer);
+        //     // if(index > -1) {
+        //     // this.observers.splice(index, 1);
+        //     // }
+
+        //     this.subjects.push( window.document.splugins.Feed.events.subject( feature_unique_key, notifications ) );
+        // }, 
+        // subscribeObserver: function(feature_unique_key, callbacks) {
+        //     // console.log("Observer " + index + " is notified!");
+
+        //     // before subscribing the ovserver check if the feature_unique_key subject is created in the first place, if not then throw error 
+        //     var found_index = null;
+        //     for(var i = 0; i < this.subjects.length; i++){
+        //         if( this.subjects[i].feature_unique_key() == feature_unique_key ) {
+
+        //             found_index = i;
+        //             break;
+        //         }
+        //     }
+
+        //     if( found_index == -1 ) {
+
+        //         throw "There is no subject exist for specified feature_unique_key "+feature_unique_key;
+        //     } else {
+
+        //         this.subjects[found_index].subscribeObserver( window.document.splugins.Feed.events.observer( callbacks ) );
+        //     }
+        // },
+        no_products_found: function() {
+
+            window.document.splugins.variation.events.core.notifyAllObservers( 'variation', 'no_products_found' );
+        }, 
+
+    } 
+}
