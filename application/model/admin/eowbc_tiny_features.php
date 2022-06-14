559797c9e0d6d24228c94bbfd037f73cc94f5ad0
ajay.bodar <ajay@emptyops.com>
2022-06-14 18:37:58 +0530

    untested

--- application/model/admin/eowbc_tiny_features.php
+++ application/model/admin/eowbc_tiny_features.php
@@ -290,12 +290,6 @@ class Tiny_Features extends Eowbc_Model {
 		
 		//	if there is any module specific js or css then it should be loaded view file or asset.php file -- both have their pros and cons. but I think asset.php is for flows var a asset file was necessary and where view is available we can simply put such js/css stuff there but in case of the admin also now we are moving the render related logic model and so maybe there will be little things in the view file 
 			// ACTIVE_TODO we may need to load above css, mostly that will going to be needed 
-
-		below hook binding should be moved to frontend class, I think d made mistake here -- to d 
-		add_filter( 'body_class',  function(){
-
-		})
-	
 		
 	}
 
