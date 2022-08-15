6457ddb91f0aee75cb469685470ed9dcb6cb68a2
Shraddha Raiyani <shraddha@emptyops.com>
2022-08-15 19:16:01 +0530

    commit@shraddha - 15-08-2022

--- templates/loop/variations-swatches/sp_variations_optionsUI-common-ribbon_wrapper.php
+++ templates/loop/variations-swatches/sp_variations_optionsUI-common-ribbon_wrapper.php
@@ -7,23 +7,24 @@
 
 
 <!--Color-->
-<div class="spui_color_widget">
-    <ul role="spui_radiogroup" aria-label="spui_color" class="spui_single_product_color_variable_items">
+<div class="spui_color_widget" -> "spui-wbc-swatches-variable-items-wrapper-div-color">
+    <ul role="spui_radiogroup"->"spui_role_type" aria-label="spui_color" class="spui_single_product_color_variable_items" ->"spui-single-product-type-variable-item ->spui-wbc-swatches-variable-items-wrapper-color">
     
     </ul>
 </div>
 
+spui-wbc-swatches-variable-items-wrapper-color
 
-<!--Size-->
-<div class="spui_size_widget">
-    <ul role="radiogroup" aria-label="spui_size" class="spui_single_product_size_variable_items spui_size_button_variable_items_wrapper">
+<!--Button-->
+<div class="spui_size_widget" -> "spui-wbc-swatches-variable-items-wrapper-div-button">
+    <ul role="radiogroup" aria-label="spui_size" class="spui_single_product_size_variable_items spui_size_button_variable_items_wrapper -> spui-wbc-swatches-variable-items-wrapper-button">
     </ul>
 </div>
 
 
 <!--Icon-->
-<div class="spui_color_icon_widget">
-    <ul role="radiogroup" aria-label="spui_color_icon" class="spui_single_product_color_icon_variable_items">
+<div class="spui_color_icon_widget" -> "spui-wbc-swatches-variable-items-wrapper-div-image" >
+    <ul role="radiogroup" aria-label="spui_color_icon" class="spui_single_product_color_icon_variable_items spui-wbc-swatches-variable-items-wrapper-type">
 
     </ul>
 </div>
