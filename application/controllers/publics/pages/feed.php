cc2c8e271a79dbb27819737cc107506ee293807d
bhavesh desai <bhavesh@emptyops.com>
2022-10-22 12:31:55 +0530

    commit@bhavesh  22-10-2022

--- application/controllers/publics/pages/feed.php
+++ application/controllers/publics/pages/feed.php
@@ -126,17 +126,19 @@ class Feed extends \eo\wbc\controllers\publics\Controller{
             remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
             \eo\wbc\model\SP_WBC_Compatibility::instance()->loop_render_compatability('before_shop_loop_item_loop_thumbnail_action', $args);
             
-            add_action( 'woocommerce_before_shop_loop_item_title', function() use($page_section,$args) {  
+            add_action( 'woocommerce_before_shop_loop_item_title', function() use($page_section,$args) {
 
-                do_action( 'sp_wbc_woo_template_loop_product_thumbnail');
+                global $product;
+                
+                do_action( 'sp_wbc_woo_template_loop_product_thumbnail',$product);
 
 
             }, 15 );
 
-            add_action('sp_wbc_woo_template_loop_product_thumbnail', function() use($page_section,$args) {  
+            add_action('sp_wbc_woo_template_loop_product_thumbnail', function($product) use($page_section,$args) {  
 
                 $args['hook_callback_args'] = array();
-                // $args['hook_callback_args']['html'] = $html;
+                $args['hook_callback_args']['product'] = $product;
                 // $args['hook_callback_args']['hook_args'] = $hook_args;
 
 
