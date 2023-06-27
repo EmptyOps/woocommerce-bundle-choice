16702a26c3f5c17386162eb5c15cd128fe22c31d
Ashish <ashish@gmail.com>
2023-06-27 19:46:26 +0530

    commit@ashish 27-06-2023

--- asset/js/common.js
+++ asset/js/common.js
@@ -3145,6 +3145,7 @@ class SP_WBC_Variations_Gallery_Images extends SP_WBC_Variations {
          // console.log( data.product_variations ); 
 
         data.types = [];
+        // ACTIVE_TODO Temp Below if false is temporary and as soon as we refectory the loading sequence for something such to ensure that we have the types available here even for the ajax variation are enabled than at that time just remove the if false and delete the else below.
         if(false) {
 
             jQuery( data.product_variations ).each(function (i, variation) {
@@ -3161,10 +3162,12 @@ class SP_WBC_Variations_Gallery_Images extends SP_WBC_Variations {
               return false;
             });
             
+        }else{
+            
+            data.types.push('image');
+            data.types.push('video');
         }
 
-        data.types.push('image');
-        data.types.push('video');
 
         return data;
     
