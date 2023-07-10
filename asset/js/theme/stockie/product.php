65682706352cc163d74d4bbdd458a14a62a4405e
Ashish <ashish@emptyops.com>
2023-07-10 14:56:18 +0530

    commit@ashish 10-07-2023

--- asset/js/theme/stockie/product.php
+++ asset/js/theme/stockie/product.php
@@ -1,4 +1,9 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossrigin="anonymous"></script> -->
+<?php 
+    // add_action('wp_enqueue_scripts', function(){
+	// 	wp_enqueue_script( 'jquery' );
+	// });
+?>
 <script>
 	jQuery(document).ready(function($){
 		let breadcrumb = $('div.eo-wbc-container.container').clone();
