09f76daa863bcd867f7f9d6d3f99c930191282d1
Shraddha Raiyani <shraddha@emptyops.com>
2023-09-02 12:14:33 +0530

    commit@krish

--- application/view/publics/breadcrumb/breadcrumb_mobile.php
+++ application/view/publics/breadcrumb/breadcrumb_mobile.php
@@ -22,10 +22,15 @@
     }
     ?>          
     <div 
-        data-href="<?php echo (  (empty(wbc()->sanitize->get('EO_CHANGE')) XOR empty(wbc()->sanitize->get('EO_VIEW'))) &&  !empty( wbc()->sanitize->get('FIRST')) && !empty(wbc()->sanitize->get('SECOND')) ? get_bloginfo('url').'/index.php'.wbc()->options->get_option('configuration','review_page')
-            .'?'.wbc()->common->http_query(array('EO_WBC'=>1,'BEGIN'=>wbc()->sanitize->get('BEGIN'),'STEP'=>3,'FIRST'=>wbc()->sanitize->get('FIRST'),'SECOND'=>wbc()->sanitize->get('SECOND'))):'#' ); ?>" 
-        class="<?php echo esc_attr((($step==3)?'active ':(($step>3)?'completed ':(!empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$clickable_breadcrumb)?'':'disabled'))))/*(($step==3)?'active ':(($step>3)?'completed ':(!empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$clickable_breadcrumb)?'':'disabled')))*/; ?> step">
-        <div class="content"><?php echo esc_html($breadcrumb_ui::$preview_name)/*$breadcrumb_ui::$preview_name*//*get_option('eo_wbc_collection_title','Preview')*/; ?></div>
+        data-href="<?php echo esc_url((empty(wbc()->sanitize->get('EO_CHANGE')) XOR empty(wbc()->sanitize->get('EO_VIEW'))) && !empty(wbc()->sanitize->get('FIRST')) && !empty(wbc()->sanitize->get('SECOND')) 
+            ? 
+            get_bloginfo('url').'/index.php'.esc_attr(wbc()->options->get_option('configuration','review_page')
+            .'?'.wbc()->common->http_query(array('EO_WBC'=>1,'BEGIN'=>wbc()->sanitize->get('BEGIN'),'STEP'=>3,'FIRST'=>wbc()->sanitize->get('FIRST'),'SECOND'=>wbc()->sanitize->get('SECOND'))))
+            :
+            '#' 
+        ); ?>" 
+        class="<?php echo esc_attr((($step==3)?'active ':(($step>3)?'completed ':(!empty(\eo\wbc\model\publics\component\EOWBC_Breadcrumb::$clickable_breadcrumb)?'':'disabled')))); ?> step">
+        <div class="content"><?php echo esc_html($breadcrumb_ui::$preview_name/*get_option('eo_wbc_collection_title','Preview')*/); ?></div>
     </div>
 </div>
 <script>
