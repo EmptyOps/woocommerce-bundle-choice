d956e717dd2637c6cb86973109fcef4d0e8ea2a5
Ashish Kakadiya <admin@admin.com>
2024-04-03 11:42:10 +0530

    commit@ashish 03-04-2024

--- application/view/ui/element/image.php
+++ application/view/ui/element/image.php
@@ -24,8 +24,34 @@
 	<?php /*NOTE: we are not escaping the $attr because it may contain multiple attributes and we do not want esc_attr function to escape the double qoutes(") of the different attribute values. but anyway we understand the security requirement here so we have always escaped the attribute value with the esc_attr from wherever this $attr var is passed*/
 	echo (!empty($attr) ? $attr : ''); ?> 
 
-	<?php /*NOTE: we are not escaping the $src var here because it contains the planceholder for the javascript template placeholders supported by the wordpress js template library under the wordpress js package window.wp*/
-	echo (!empty($src) ? 'src="' . ( (str_contains($src,'{{data.') && substr($src, -2)  == "}}") ? $src : esc_url($src) ) . '"' : ''); ?> 
+	<?php // echo (!empty($src) ? 'src="' . ( (str_contains($src,'{{data.') && substr($src, -2)  == "}}") ? $src : esc_url($src) ) . '"' : ''); 
+	if (!empty($src)) {
+		
+		echo 'src="';
+		
+		if ($src == '{{data.src}}') {
+
+			echo '{{data.src}}"';
+
+		} elseif ($src == '{{data.full_src}}') {
+			
+			echo '{{data.full_src}}"';
+		
+		} elseif ($src == '{{data.video_src}}') {
+			
+			echo '{{data.video_src}}"';
+		
+		} elseif ($src == '{{data.gallery_thumbnail_src}}') {
+			
+			echo '{{data.gallery_thumbnail_src}}"';
+		
+		} else {
+
+			echo esc_url($src) . '"';
+
+		}
+	}
+	?> 
 	<?php echo (!empty($style) ? 'style="' . esc_attr($style) . '"' : ''); ?> 
 />
 
