d46f398b97c375d6659b4fcdc1de31fd7abbfd85
hi0001234d <hi0001234d@gmail.com>
2024-04-15 14:27:28 +0530

    Update sample-data-template.php

--- application/model/admin/sample_data/data_templates/sample-data-template.php
+++ application/model/admin/sample_data/data_templates/sample-data-template.php
@@ -15337,7 +15337,7 @@ class Sample_Data_Template {
 
                 } else {
 
-                    return array("type"=>"error", "msg"=>"Save prosses failed for one of the item.");
+                    return array("type"=>"error", "msg"=>"Save process failed for one of the item.");
                 }
 
             } else {
