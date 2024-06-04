8d607e30972d85124471dca9d3e5e96005818bfa
prince <prince@emptyops.com>
2024-06-04 18:51:17 +0530

    commit@prince - 04/06/2024

--- application/controllers/ajax/eowbc_extras.php
+++ application/controllers/ajax/eowbc_extras.php
@@ -12,11 +12,11 @@ if (wp_verify_nonce(wbc()->sanitize->post('_wpnonce'), 'eowbc_extras')) {
 	wbc()->load->model('admin\form-builder');
 
 	if (isset($_POST["sub_action"]) && wbc()->sanitize->post("sub_action") == "bulk_delete") {
-		$res = eo\wbc\model\admin\Eowbc_Extras::instance()->delete(wbc()->sanitize->post_array("ids"), wbc()->sanitize->post("saved_tab_key"));
+		$res = \eo\wbc\model\admin\Eowbc_Extras::instance()->delete(wbc()->sanitize->post_array("ids"), wbc()->sanitize->post("saved_tab_key"));
 	} elseif (isset($_POST['sub_action']) and wbc()->sanitize->post('sub_action') == 'fetch') {
-		$res = eo\wbc\model\admin\Eowbc_Extras::instance()->fetch_map($res);
+		$res = \eo\wbc\model\admin\Eowbc_Extras::instance()->fetch_map($res);
 	} else {
-		$res = eo\wbc\model\admin\Eowbc_Extras::instance()->save(eo\wbc\controllers\admin\menu\page\Extras::get_form_definition());
+		$res = \eo\wbc\model\admin\Eowbc_Extras::instance()->save(\eo\wbc\controllers\admin\menu\page\Extras::get_form_definition());
 	}
 
 } else {
