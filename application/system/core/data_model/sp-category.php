32252044dbbd8c42030ebef09fd3547fb4db3e13
kirtan <kirtan@emptyops.com>
2025-09-01 12:36:28 +0530

    kirtan

--- application/system/core/data_model/sp-category.php
+++ application/system/core/data_model/sp-category.php
@@ -21,15 +21,16 @@ class SP_Category extends SP_Entity {
 		throw new Exception("Sorry, singleton instance method not supported for this class. Always use construct method to create object.", 1);
 	}
 
+	// NOTE: only construct method with platform_key etc parameters is supported, so pass platform_key etc parameters as parameters to construct method. Default construct method is not supported.
 	public function __construct( $platform_key, $platform_name ) {
 
 		$this->platform_key = $platform_key;
 		$this->platform_name = $platform_name;
 	}
 
-	public function __construct() {
-		throw new Exception("Sorry, only construct method with platform_key etc parameters is supported, so pass platform_key etc parameters as parameters to construct method. Default construct method is not supported.", 1);
-	}
+	// public function __construct() {
+	// 	throw new Exception("Sorry, only construct method with platform_key etc parameters is supported, so pass platform_key etc parameters as parameters to construct method. Default construct method is not supported.", 1);
+	// }
 
 	public static function createFromJson(){
 		throw new Exception("Set method is not supposed to be supported for this property, rely on construct method to set this property.", 1);
