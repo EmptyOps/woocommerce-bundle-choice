1b8ad75443d93db8f39cab088e35d7c6dc193fdf
kirtan <kirtan@emptyops.com>
2025-09-01 13:32:32 +0530

    kirtan

--- application/view/publics/category.php
+++ application/view/publics/category.php
@@ -3,8 +3,8 @@
 /*
 *	Template to show category page
 */
-<!-- Created with Wordpress plugin - BUNDLOICE (formerly Woo Choice Plugin) -->
-    //NOTE:From here, we have removed the original code inside the if (false) block.So, whenever there is a need to view the original or any other code for readability purposes, simply take the css below, put it in a new .css file in Sublime Text,and view it in readable format.Apart from that, we had removed the original code, and in some scenarios,that original code might have contained PHP variables like XYZ. Those would have been removed as well. And of course, even if the removed code from the if (false) block is not relevant to the current version,it might be required during future milestone tasks, so for this purpose,refer to the branch named "ui_QCed_ashish_-2" and check the commit dated 07-04-2025 for looking at the original code.
+    // <!-- Created with Wordpress plugin - BUNDLOICE (formerly Woo Choice Plugin) -->
+    // NOTE:From here, we have removed the original code inside the if (false) block.So, whenever there is a need to view the original or any other code for readability purposes, simply take the css below, put it in a new .css file in Sublime Text,and view it in readable format.Apart from that, we had removed the original code, and in some scenarios,that original code might have contained PHP variables like XYZ. Those would have been removed as well. And of course, even if the removed code from the if (false) block is not relevant to the current version,it might be required during future milestone tasks, so for this purpose,refer to the branch named "ui_QCed_ashish_-2" and check the commit dated 07-04-2025 for looking at the original code.
     $custom_css = "
         .cat_products{
             border: 1.3px solid #80808059;
