6284c87b70246f491acd1f7a5022beb5ffa8cb44
DAnn2012 <dannunziata@hotmail.it>
2025-10-06 17:34:13 +0200

    Update form.php

--- application/view/core/ui/components/form.php
+++ application/view/core/ui/components/form.php
@@ -2,7 +2,7 @@
 // Semantic UI : form
 ?>
 
-<form class="ui form <?php !empty($class) ? esc_attr_e($class) : ''; ?>" id="<?php !empty($id) ? esc_attr_e($id) : ''; ?>" name="<?php !empty($name) ? esc_attr_e($name) : ''; ?>" method="<?php !empty($method) ? esc_attr_e($method) : ''; ?>" <?php  !empty($style) ? 'style="'.esc_attr_e($style).'"' : ''; ?>>
+<form class="ui form <?php echo (!empty($class) ? esc_attr($class) : ''); ?>" id="<?php echo (!empty($id) ? esc_attr($id) : ''); ?>" name="<?php echo (!empty($name) ? esc_attr($name) : ''); ?>" method="<?php echo (!empty($method) ? esc_attr($method) : ''); ?>" <?php  echo (!empty($style) ? 'style="'.esc_attr($style).'"' : ''); ?>>
 	<?php 
 		if(!empty($builder) and !empty($child)){
 			$builder->build($child,$option_key,$process_form);
