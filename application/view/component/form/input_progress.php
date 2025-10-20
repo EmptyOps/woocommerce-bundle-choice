944f0427e08eaeb637f33fe8abed7312eacbd945
smit <tejasr@emptyops.com>
2025-10-20 15:38:08 +0530

    smit@emptyops

--- application/view/component/form/input_progress.php
+++ application/view/component/form/input_progress.php
@@ -6,6 +6,6 @@
 
 if(!empty($id) /*and !empty($label)*/){
 	?>	
-	<progress id="<?php echo esc_attr($id); ?>" value="<?php echo !empty($value)?$value:0; ?>" min="0" max="100" style="width:100%;height:14px;display:block;margin-bottom:5px;"> </progress>
+	<progress id="<?php echo esc_attr($id); ?>" value="0" min="0" max="100" style="width:100%;height:14px;display:none;margin-bottom:5px;"> </progress>
 	<?php
 }
