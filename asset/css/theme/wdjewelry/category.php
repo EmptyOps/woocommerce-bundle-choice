6014ee702b9cb07e44bb6ccaefdaa95c13c2bc4f
bhavnesh75 <bhavnesh@emptyops.com>
2025-11-24 15:13:06 +0530

    bhavnesh 24/11/2025

--- asset/css/theme/wdjewelry/category.php
+++ asset/css/theme/wdjewelry/category.php
@@ -28,6 +28,7 @@ if(WBC_SCRIPT_DEBUG == true){
     <style type="text/css">
         .products>li{visibility:visible!important}@media (min-width:768px){.col-sm-18{width:100%!important}}#reset_filter{left:2em!important}#apply_filter{right:2em!important}.products.grid>li{max-width:22%!important}
     </style>
+<?php add_filter('eowbc_filter_sidebars_widgets','__return_false'); ?>    
     
 <?php
 }
