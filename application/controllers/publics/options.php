b8564f4f0ed5fd7e9d8bba58ff1136a529979535
DAnn2012 <dannunziata@hotmail.it>
2025-12-04 11:33:10 +0100

    Update options.php

--- application/controllers/publics/options.php
+++ application/controllers/publics/options.php
@@ -711,7 +711,7 @@ class Options extends \eo\wbc\controllers\publics\Controller {
 						// wbc()->load->asset('css','fomantic/semantic.min');
 						// wbc()->load->asset('js','fomantic/semantic.min',array('jquery'));
 						wbc()->load->built_in_asset('semantic');
-						$toggle_text = esc_html_e($toggle_text);
+						$toggle_text = esc_html__($toggle_text);
 
 						// NOTE:From here, we have removed the original code inside the if (false) block. So, whenever there is a need to view the original or any other code for readability purposes, simply take the script below, put it in a new .js file in Sublime Text, and view it in readable format.Apart from that, we had removed the original code, and in some scenarios, that original code might have contained PHP variables like XYZ. Those would have been removed as well.And of course, even if the removed code from the if (false) block is not relevant to the current version, it might be required during future milestone tasks, so for this purpose, refer to the branch named "ui_QCed_ashish_-2" and check the commit dated 07-04-2025 for looking at the original code.
 						$inline_script = 
